52816e112146b8b354b85666916a543ab524b81f
ArchAngel776 <karol.marciniak155@gmail.com>
2021-05-29 12:36:38 +0200

    Update DiameterValidator.tsx

--- src/App/Validators/DiameterValidator.tsx
+++ src/App/Validators/DiameterValidator.tsx
@@ -4,7 +4,7 @@ export default class DiameterValidator extends Validator {
 
     protected setup() : void {
 
-        this.isFloat("Diameter must a number");
+        this.isFloat("Diameter must be a number");
 
         this.min(0, "Sory but this diameter is impossible to make (How do you think why?)");
 
